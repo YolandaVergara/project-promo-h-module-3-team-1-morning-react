ebcd95c70067652faf9ca62946a33f1c7c02fb50
Yolanda <yv.avila@gmail.com>
2019-12-18 11:09:48 +0100

    change data component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,43 @@
 import React from 'react';
-import FormElement from './FormElement';
-import dataItems from './data/dataItems.js';
+import FormText from './FormText';
+import dataItems from './data/dataItems.json';
 
 
-class Form extends React.Component{
-  render() {    
-      return ( 
+class Form extends React.Component {
+  render() {
+    return (
       dataItems.map((dataItem, key) => {
         console.log(dataItem);
-        return (<FormElement key={key} 
-          
-              htmlFor={dataItem.htmlFor}
-              label={dataItem.label}
-              type={dataItem.type}
-              id={dataItem.id}
-              name={dataItem.name}
-              placeholder={dataItem.placeholder}
-              maxlenght={dataItem.maxlenght}
-              title={dataItem.title}
-              />      
-            )
-          }
-      ) 
-    
-  )
-}
+        if (dataItem.component === 'FormText') {
+          return (<FormText key={key}
+            htmlFor={dataItem.htmlFor}
+            label={dataItem.label}
+            type={dataItem.type}
+            id={dataItem.id}
+            name={dataItem.name}
+            placeholder={dataItem.placeholder}
+            maxlenght={dataItem.maxlenght}
+            title={dataItem.title}
+          />
+          );
+        } else {
+          return (<FormText key={key}
+            htmlFor={dataItem.htmlFor}
+            label={dataItem.label}
+            type={dataItem.type}
+            id={dataItem.id}
+            name={dataItem.name}
+            placeholder={dataItem.placeholder}
+            maxlenght={dataItem.maxlenght}
+            title={dataItem.title}
+          />
+          )
+        }
+      }
+      )
+
+    )
+  }
 }
 
 export default Form;    
