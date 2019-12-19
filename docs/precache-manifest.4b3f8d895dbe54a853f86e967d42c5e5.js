653cad482beac6be8ec0a6954e6855706e0c4298
elenapayan <elena.payan.glez@gmail.com>
2019-12-19 13:06:51 +0100

    Add docs script

---
+++ docs/precache-manifest.4b3f8d895dbe54a853f86e967d42c5e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43156f6c1f1fab1732ac2d3e140744dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8656555918d949e6b9fe",
+    "url": "/static/css/main.25b2f4f8.chunk.css"
+  },
+  {
+    "revision": "afb88be9faeb48787cf8",
+    "url": "/static/js/2.89170efe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.89170efe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8656555918d949e6b9fe",
+    "url": "/static/js/main.87a5df53.chunk.js"
+  },
+  {
+    "revision": "d33bbe409c884cb500a0",
+    "url": "/static/js/runtime-main.56ff1ea5.js"
+  },
+  {
+    "revision": "bca7b1c5eaaff47ef62e9c8c0082c3ef",
+    "url": "/static/media/fondoRosa.bca7b1c5.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "/static/media/logo-awesome.0118e311.svg"
+  },
+  {
+    "revision": "a3110a85b0e3d318c46a84ee17f5b7ea",
+    "url": "/static/media/logo-card.a3110a85.png"
+  },
+  {
+    "revision": "b374a1588dfc3f04026981775398b4bf",
+    "url": "/static/media/paquita.b374a158.jpg"
+  }
+]);
