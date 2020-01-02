97db6f9840418fa3347a256a4c37a2c483d67600
Elva Souto Fernández <e.souto.fernandez@gmail.com>
2020-01-02 11:51:50 +0100

    add propTypes to Palette.js

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,12 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Palette extends React.Component {
   render() {
     return (
       <React.Fragment>
         <div className="form__palette--1">
-          <label className="label" htmlFor={this.props.htmlFor}></label>
-          <input value={this.props.value} type={this.props.type} name={this.props.name} id={this.props.id} className="design__form--radio js-form-palette1" defaultChecked={this.props.defaultChecked} />
+          <label className="label" htmlFor={this.props.id}></label>
+          <input
+            value={this.props.value}
+            type={this.props.type}
+            name={this.props.name}
+            id={this.props.id}
+            className="design__form--radio js-form-palette1"
+            defaultChecked={this.props.defaultChecked}
+          />
           <ul className="palette-color">
             <li className={this.props.color1}></li>
             <li className={this.props.color2}></li>
@@ -17,5 +25,16 @@ class Palette extends React.Component {
     );
   }
 }
+
+Palette.propTypes = {
+  value: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  defaultChecked: PropTypes.string,
+  color1: PropTypes.string,
+  color2: PropTypes.string,
+  color3: PropTypes.string
+};
 
 export default Palette;
